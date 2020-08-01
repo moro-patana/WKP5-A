5a2a5111d30262f14c9bb94a4828f8121099d796
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-08-01 16:16:16 +0300

    changed the id in the array object

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ const recipes = [
 			'Add some salt on it',
 			'Wait, put them out',
 		],
-		id: 1596168522409,
+		id: 1596168522402,
 	},
 	{
 		title: 'Salad',
@@ -50,6 +50,7 @@ const recipes = [
 		id: 1596168522409,
 	},
 ];
+// Grab elements
 const container = document.querySelector(`.container`)
 const postList = document.querySelector(`#post-list`);
 const heading = document.querySelector(`h1`);
@@ -62,19 +63,38 @@ const innerModal = document.querySelector(`.inner-modal`);
 
 
 const renderCard = () => {
-// 	// check the recipes collection
-// 	function getArrayOfRecipes(obj) {
-// 		return Object.keys(recipes);
-// 	}
-// 	console.log(getArrayOfRecipes(recipes));
+// check the recipes collection
+	function getArrayOfRecipes(obj) {
+		return Object.keys(recipes);
+	}
+	console.log(getArrayOfRecipes(recipes));
 
-// recipes.forEach(function(element) {
-// 	console.log(element);
-// });
-	// generate the HTML
+recipes.forEach(function(element) {
+	console.log(element);
+});
+
+ // Loop through the object
 for (let i = 0; i < recipes.length; i++) {
+
+    // Grab the ingredients array value into list item
+	let ingredientsValue = recipes[i].ingredients;
+	let ingredientsTxt = "";
+
+	ingredientsValue.forEach(myFunction);
+	function myFunction(item) {
+		ingredientsTxt += "<li>" + item + "</li>";
+	}
+// let stepsValue = recipes[i].steps;
+// let stepsTxt = "";
+
+// stepsValue.forEach(myFunction);
+// function myFunction(item) {
+// stepsTxt += "<li>" + item + "</li>";
+// }
+
+   // generate the HTML
 	const myHTML = `
-	    <article class="card">
+	    <article class="card" data-id="${recipes[i].id}">
 				<h2 class="card-title">${recipes[i].title}</h2>
 				<img src="${recipes[i].picture}">
 				<nav>
@@ -93,52 +113,49 @@ container.insertAdjacentHTML("beforeend", myHTML);
 }
 }
 
-const moreInfo = () => {
-	// const {title, picture, author, difficulty, ingredients, steps} = cardElement;
-	// cardElement.querySelector(`.card`).textContent;
-	const moreInfoHTML = `
-	<h4>${recipes.title} by</h4>
-	  <img src="">
+  // Create a modal
+const moreInfo = (recipe) => {
+innerModal.innerHTML = `
+	<h4>${recipe.title} by ${recipe.author}</h4>
+	  <img src="${recipe.picture}">
 	  <nav>
-	  <ul>
-		<li>Timing: </li>
-		<li>Difficulty: </li>
-		<li>Ingredients:
-			<ul>
-				<li>
-				
-				</li>
+	  <ul class="list">
+		<li><b>Timing:</b> ${recipe.timing}</li>
+		<li><b>Difficulty:</b> ${recipe.difficulty}</li>
+		<li><b>Ingredients:</b>
+			<ul class="ingredients">
 			</ul>
 		</li>
 		<li>
 			<ul>
-				<li>
-				
+				<li><b>Steps:</b>
+				${recipe.steps}
 				</li>
 			</ul>
 		</li>
 	  </ul>
 	  </nav>
 	`; 
-	innerModal.innerHTML = moreInfoHTML;
 	outerModal.classList.add(`open`);
-  }
-
+	 }
+  // Add class
   const handleCloseModal = event => {
     const isOutside = !event.target.closest(`.inner-modal`);
     if (isOutside) {
         outerModal.classList.remove(`open`);
     }
 };
-
+ // Remove the class
 const handleEscapeKey = event => {
     if (event.key === `Escape`) {
         closeModal();
     }
 };
+
 const closeModal = event => {
     outerModal.classList.remove(`open`);
 }
+
   const handleBtnClick = event => {
 	if (event.target.matches(`button.info`)) {
 		const card = event.target.closest(`.card`);
@@ -147,14 +164,140 @@ const closeModal = event => {
 		moreInfo(recipe);
 	}
   };
+// Listen to the add a new recipe button
+  const handleAddRecipe = (event) => {
+// Create a form
+	  innerModal.innerHTML = `
+	  <div class="col-8 offset-2">
+	  <div class="card" id="form-card">
+			  <form class="post-form">
+				  <h2>Add a new cookbook</h2>
+				  <div class="form-group">
+					  <label for="recipeName">What's the recipe name?</label><br>
+					  <input
+						  type="text"
+						  class="form-control"
+						  id="recipeName"
+						  name="recipeName"
+						  placeholder="Enter recipe name"
+					  />
+				  </div>
+				  <div class="form-group">
+					  <label for="resultImage">Picture of the result (URL)</label><br>
+					  <input
+						  class="form-control"
+						  type="url"
+						  name="resultImage"
+						  rows="3"
+						  placeholder="Enter image url"
+					  />
+				  </div>
+				  <div class="form-group">
+					  <label for="cookName">Who's the cook?</label><br>
+					  <input
+						  type="text"
+						  class="form-control"
+						  name="cookName"
+						  aria-describedby="cookName"
+						  placeholder="Enter cook's name"
+					  />
+				  </div>
+				  <div class="form-group">
+					  <label for="difficulty">What's the difficulty?</label><br>
+						  <select id="difficulty-select" name="difficulty">
+							  <option value="easy">Easy</option>
+							  <option value="quite difficult">Quite difficult</option>
+							  <option value="very difficult">Very difficult</option>
+						  </select>
+				  </div>
+				  <div class="form-group">
+					  <label for="time">How much time does it take?</label><br>
+						  <select id="time-select" name="timing">
+							  <option value="">Less than 15 minutes</option>
+							  <option value="30min">30 minutes</option>
+							  <option value="45min">40 minutes</option>
+							  <option value="1hour">An hour</option>
+							  <option value="more">More than an hour</option>
+						  </select>
+				  </div>
+				  <div class="form-group">
+					  <label for="ingredient">Ingredients</label><br>
+					  <ul id="ingredients">
+						  <li>
+							  <input 
+							  type="text" 
+							  id="ingredient" 
+							  name="ingredient" 
+							  value="eggs">
+						  </li>
+					  </ul>
+					  <button class="add-ingredient">Add a new ingredient to the list</button>
+				  </div>
+				  <div class="form-group">
+					  <label for="steps">Steps</label><br>
+					  <ul id="steps">
+						  <li>
+							  <input 
+							  type="text" 
+							  id="steps" 
+							  name="steps" 
+							  value="fried the eggs">
+						  </li>
+					  </ul>
 
+					  <button class="add-step">Add a new step to the list</button>
+				  </div>
+				  <button type="submit" class="btn btn-primary">
+					  Submit
+				  </button>
+			  </form>
+		  </div>
+  </div>
+`;
+outerModal.classList.add(`open`);
+  }
+// Add event listener to the submit button
+const handleSubmitBtn = (event) => {
+	event.preventDefault();
+	if (event.target.matches(`form`)) {
+		const form = event.target;
+		const title = form.title.value;
+		const picture = form.resultImage.value;
+		const cookName = form.cookName.value;
+		const time = form.timing.value
+		const difficulty = form.difficulty.value;
+		const ingredients = form.ingredient.value;
+		const steps = form.steps.value;
+// Create an html for the result of the from
+		const newRecipe = `
+		<article class="card" data-id="${recipes.id}">
+		<h2 class="card-title">${title}</h2>
+		<img src="${picture}">
+		<nav>
+		  <ul>
+			<li>Timing: ${time}</li>
+			<li>Difficulty: ${difficulty}</li>
+		  </ul>
+		</nav>
+		<button type="button" class="info">
+		More info
+		</button>
+</article>
 
+`;
+container.innerHTML += newRecipe;
+outerModal.classList.remove(`open`);
+form.reset();
+	}
+
+} 
 
 
   // Listeners
+  window.addEventListener(`submit`, handleSubmitBtn);
   window.addEventListener(`click`, handleBtnClick);
   window.addEventListener(`keydown`, handleEscapeKey);
   outerModal.addEventListener(`click`, handleCloseModal);
-//   addOrderBtn.addEventListener(`click`, addOrder);
 
 generateButton.addEventListener('click', renderCard);
+addNewRecipe.addEventListener(`click`, handleAddRecipe);
